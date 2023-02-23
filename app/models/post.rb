398594570b36303be16e14d19ab3f09b3ca0822a
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2023-02-23 21:33:34 +0430

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes
+
+  def upadate_countr
+    if user.posts_countr
+      user.posts_counter += 1
+    else
+      user.posts_counter = 1
+    end
+    user.save
+  end
+
+  def most_recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
 end
