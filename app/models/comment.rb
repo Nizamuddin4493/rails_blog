8e7a93202a0fbeb5b91950bae659497e76ecb1ad
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2023-02-25 18:50:53 +0430

    adding test for comment Model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_counter
-    if post.commentsCounter
-      post.commentsCounter += 1
-    else
-      post.commentsCounter = 1
-    end
-    post.save
-    post.save
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
 end
