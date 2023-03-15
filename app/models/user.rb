885088a490745ff9148b7ee1888eb57ac53e096d
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-15 17:25:12 +0630

    Update the layout header

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :postsCounter,
             numericality: { only_interger: true, greater_than_or_equal_to: 0,
-                            message: 'Must be integer and greater than or Equal to Zero.' }
+                            message: 'Must be integer and greater than or Equal to Zero.' }, allow_nil: true
 
   def last_3_posts
     posts.last(3)
